092c02f509da73959a6e713a9be33ca72f450768
mfghir <fatemeghafari77@gmail.com>
2024-04-10 14:53:16 +0330

    test 8 - mongo url

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://MONGO_USER=fatemeghafari77:NoERisX99wBrN4Bl@cluster0.xhmztka.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://fatemeghafari77:NoERisX99wBrN4Bl@cluster0.xhmztka.mongodb.net/?retryWrites=true&w=majority");
     // await mongoose.connect(process.env.MONGO_URI);
     console.log("connect to DB");
   } catch (error) {
