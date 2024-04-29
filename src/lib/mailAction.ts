072c2489402a8b54684c3dc20674184b902f997f
mfghir <fatemeghafari77@gmail.com>
2024-04-29 17:28:32 +0330

    test 2 - mailtrap

--- src/lib/mailAction.ts
+++ src/lib/mailAction.ts
@@ -4,6 +4,17 @@ import User from "@/models/user";
 import connectDB from "./connectDB";
 import nodemailer from "nodemailer";
 
+import { MailtrapClient } from "mailtrap";
+
+
+const TOKEN = "5d31785dea64bd3fe1d817c7757477c6";
+const ENDPOINT = "https://send.api.mailtrap.io/";
+
+const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+
+
+
+
 export async function mailAction({ email }: { email: any }) {
   await connectDB();
   const user = await User.findOne({ email });
@@ -15,6 +26,20 @@ export async function mailAction({ email }: { email: any }) {
     );
     // console.log("token " ,token)
 
+    const sender = {
+      email: "mailtrap@kdrama-list.vercel.app",
+      name: "Mailtrap Test",
+    };
+    const recipients = [
+      {
+        email: "fatemeghafari77@gmail.com",
+      },
+    ];
+  
+
+
+
+
     // const transporter = nodemailer.createTransport({
     //   host: "sandbox.smtp.mailtrap.io",
     //   port: 2525,
@@ -24,26 +49,54 @@ export async function mailAction({ email }: { email: any }) {
     //   },
     // });
 
-    const transporter = nodemailer.createTransport({
-      host: "live.smtp.mailtrap.io",
-      port: 587,
-      auth: {
-        user: "api",
-        pass: "5d31785dea64bd3fe1d817c7757477c6",
-        // user: process.env.MAIL_USER,
-        // pass: process.env.MAIL_PASS,
-      },
-    });
+  
+  
 
-    const htmlBody = `Click here to <a  href="http://localhost:3000/reset-password/${token}">Resat password</a> `;
-    const info = await transporter.sendMail({
-      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: htmlBody, // html body
-    });
-    console.log("Message sent: %s", info.messageId);
+    
+
+    // const transporter = nodemailer.createTransport({
+    //   host: "live.smtp.mailtrap.io",
+    //   port: 587,
+    //   auth: {
+    //     user: "api",
+    //     pass: "5d31785dea64bd3fe1d817c7757477c6",
+    //     // user: process.env.MAIL_USER,
+    //     // pass: process.env.MAIL_PASS,
+    //   },
+    // });
+
+    // const htmlBody = `Click here to <a  href="http://localhost:3000/reset-password/${token}">Resat password</a> `;
+    // const info = await transporter.sendMail({
+    //   from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+    //   to: "bar@example.com, baz@example.com", // list of receivers
+    //   subject: "Hello ✔", // Subject line
+    //   text: "Hello world?", // plain text body
+    //   html: htmlBody, // html body
+    // });
+    // console.log("Message sent: %s", info.messageId);
+
+
+
+
+    try {
+      const result = await client.send({
+        from: sender,
+        to: recipients,
+        subject: "You are awesome!",
+        text: "Congrats for sending test email with Mailtrap!",
+        category: "Integration Test",
+      });
+  
+      console.log(result);
+    await User.findOneAndUpdate({ email: email }, { verifyToken: token });
+
+      // res.status(200).json({ message: "Email sent successfully" });
+    } catch (error) {
+      console.error(error);
+      // res.status(500).json({ error: "Failed to send email" });
+    }
+
+
 
     // save in db
     await User.findOneAndUpdate({ email: email }, { verifyToken: token });
@@ -53,3 +106,35 @@ export async function mailAction({ email }: { email: any }) {
     throw new Error("User not found");
   }
 }
+
+
+
+
+
+// export default async function handler(req, res) {
+//   const sender = {
+//     email: "mailtrap@kdrama-list.vercel.app",
+//     name: "Mailtrap Test",
+//   };
+//   const recipients = [
+//     {
+//       email: "fatemeghafari77@gmail.com",
+//     },
+//   ];
+
+//   try {
+//     const result = await client.send({
+//       from: sender,
+//       to: recipients,
+//       subject: "You are awesome!",
+//       text: "Congrats for sending test email with Mailtrap!",
+//       category: "Integration Test",
+//     });
+
+//     console.log(result);
+//     res.status(200).json({ message: "Email sent successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to send email" });
+//   }
+// }
